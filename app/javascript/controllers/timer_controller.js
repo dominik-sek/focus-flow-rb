5aae50f290392dbca26607e38d502291af380316
gothic459 <d.sek464@gmail.com>
2025-08-03 09:04:30 +0200

    add timer controller actions

--- app/javascript/controllers/timer_controller.js
+++ app/javascript/controllers/timer_controller.js
@@ -1,15 +1,65 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ "timerDisplay" ]
+  static targets = ["timerDisplay", "toggleButton"]
+  timerInterval = null
+  hours = 0
+  minutes = 0
+  seconds = 0
+  isRunning = false
 
+  reset() {
+    this.hours = 0
+    this.minutes = 0
+    this.seconds = 0
+    this.updateDisplay()
+    if (this.timerInterval) {
+      clearInterval(this.timerInterval)
+      this.timerInterval = null
+    }
+    this.isRunning = false
+    this.toggleButtonTarget.textContent = "Start"
+  }
   connect() {
-    console.log("TimerController connected", this.element);
-    console.log(this.timerDisplayTarget);
-    
+    this.reset()
+    console.log("TimerController connected", this.element);    
+  }
+  toggle() {
+    if (this.isRunning) {
+      this.stop();
+    } else {
+      this.start();
+    }
+    //this.isRunning = !this.isRunning;
   }
   start() {
-    console.log("Timer started");
-    this.timerDisplayTarget.textContent = "00:00:01"
+    this.reset()
+    this.toggleButtonTarget.textContent = "Stop";
+    this.isRunning = true;
+    this.timerInterval = setInterval(() => {
+      this.seconds++;
+      if (this.seconds >= 60) {
+        this.seconds = 0;
+        this.minutes++;
+      }
+      if (this.minutes >= 60) {
+        this.minutes = 0;
+        this.hours++;
+      }
+      this.updateDisplay();
+    }, 1000);
+
+  }
+  updateDisplay() {
+    const hours = String(this.hours).padStart(2, '0');
+    const minutes = String(this.minutes).padStart(2, '0');
+    const seconds = String(this.seconds).padStart(2, '0');
+    this.timerDisplayTarget.textContent = `${hours}:${minutes}:${seconds}`;
+  }
+
+  stop() {
+    window.clearInterval(this.timerInterval);
+    this.isRunning = false;
+    this.toggleButtonTarget.textContent = "Start";
   }
 }
