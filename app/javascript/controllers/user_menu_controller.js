811d541baf938de4ce78c4c3326c37487da06aa7
p <d.sek464@gmail.com>
2025-08-05 17:56:23 +0200

    add outside click detection for menu, close menu on outside click

--- app/javascript/controllers/user_menu_controller.js
+++ app/javascript/controllers/user_menu_controller.js
@@ -2,20 +2,38 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="user-menu"
 export default class extends Controller {
-  static targets = ["menu", "dropdownMenu"]
+  static targets = ["dropdownMenu"]
   isOpen = false
-
   connect() {
+    this.checkIfOutsideClick = this.checkIfOutsideClick.bind(this)
     console.log("UserMenuController connected")
   }
+
+  disconnect() {
+    document.removeEventListener("click", this.handleOutsideClick)
+
+  }
+
+
+
   toggle() {
     this.isOpen = !this.isOpen
     if (this.isOpen) {
+      document.addEventListener("click", this.checkIfOutsideClick)
       this.dropdownMenuTarget.classList.remove("hidden")
-      //this.dropdownMenuTarget.classList.add("absolute")
     } else {
-      this.dropdownMenuTarget.classList.add("hidden")
-      //this.dropdownMenuTarget.classList.remove("absolute")
+      this.close()
     }
   }
+  close() {
+    this.isOpen = false
+    this.dropdownMenuTarget.classList.add("hidden")
+    document.removeEventListener("click", this.checkIfOutsideClick)
+  }
+  checkIfOutsideClick(e) {
+    if (!this.element.contains(e.target)) {
+      this.close()
+    }
+  }
+
 }
