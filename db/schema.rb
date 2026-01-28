88c0add11a6459a28be18c4d561682cbd1003ef0
gothic459 <d.sek464@gmail.com>
2026-01-28 19:02:07 +0100

    update reails, run npm audit fix

--- db/schema.rb
+++ db/schema.rb
@@ -10,47 +10,38 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_05_195636) do
-  create_schema "dziekanat"
-  create_schema "kadry"
-  create_schema "kursory"
-  create_schema "poczta_olap"
-  create_schema "poczta_oltp"
-  create_schema "rodo"
-  create_schema "roznosci"
-  create_schema "wf"
-
+ActiveRecord::Schema[8.1].define(version: 2026_01_14_192215) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "btree_gist"
   enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
 
   create_table "projects", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "time_entries", force: :cascade do |t|
-    t.string "name"
-    t.datetime "started_at", precision: nil
-    t.datetime "finished_at", precision: nil
-    t.integer "duration"
     t.datetime "created_at", null: false
+    t.integer "duration"
+    t.datetime "finished_at", precision: nil
+    t.string "name"
+    t.bigint "project_id"
+    t.datetime "started_at", precision: nil
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.bigint "project_id"
     t.index ["project_id"], name: "index_time_entries_on_project_id"
     t.index ["user_id"], name: "index_time_entries_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.datetime "last_login"
     t.datetime "created_at", null: false
+    t.string "email"
+    t.datetime "last_login"
+    t.string "password_digest"
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
